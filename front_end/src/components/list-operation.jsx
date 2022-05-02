2769b7f73c518ac002680bf7f5c948f2c3631e6c
Darlinson Alves Pereira <darlinsonalves@brq.com>
2022-05-02 15:05:57 -0300

    lista de operações...

--- front_end/src/components/list-operation.jsx
+++ front_end/src/components/list-operation.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import moment from 'moment';
+
 import { apiRequest } from "../helpers/api.helpers";
+import { formatter } from "../helpers/utils.helper";
 
 export const ListOperations = () => {
+
+  const [operations, setOperations] = React.useState([])
 
   const getList = () => {
     apiRequest.get('stores/operations')
       .then(res => {
         console.log('33', res)
+        setOperations(res.operations)
       }).catch(err => {
         console.log('err', err)
       })
@@ -19,64 +25,40 @@ export const ListOperations = () => {
 
   return (
     <div>
-      <h1>A Fancy Table</h1>
+      <select type="text" id="myInput" onkeyup="myFunction()" placeholder="Pesquisar pelo nome Dono" title="Type in a name"></select>
+      <h1 className="tb-title">Lista de Operações</h1>
 
-      <table id="customers">
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
+      <table id="myTable">
+        <tr class="header">
+          <th>Tipo</th>
+          <th>Data</th>
+          <th>Valor</th>
+          <th>CPF</th>
+          <th>Cartão</th>
+          <th>Hora</th>
+          <th>Dono da Loja</th>
+          <th>Nome Loja</th>
         </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
+        <tr class="header">
+          <td colSpan={8} >saldo: </td>
         </tr>
-        <tr>
-          <td>Berglunds snabbköp</td>
-          <td>Christina Berglund</td>
-          <td>Sweden</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
-        <tr>
-          <td>Ernst Handel</td>
-          <td>Roland Mendel</td>
-          <td>Austria</td>
-        </tr>
-        <tr>
-          <td>Island Trading</td>
-          <td>Helen Bennett</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td>Königlich Essen</td>
-          <td>Philip Cramer</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Laughing Bacchus Winecellars</td>
-          <td>Yoshi Tannamuri</td>
-          <td>Canada</td>
-        </tr>
-        <tr>
-          <td>Magazzini Alimentari Riuniti</td>
-          <td>Giovanni Rovelli</td>
-          <td>Italy</td>
-        </tr>
-        <tr>
-          <td>North/South</td>
-          <td>Simon Crowther</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td>Paris spécialités</td>
-          <td>Marie Bertrand</td>
-          <td>France</td>
-        </tr>
+        {
+          operations.map(operation => {
+            const { type } = operation
+            return (
+            <tr>
+              <td>{type.description}</td>
+              <td>{moment(operation.data).format('DD/MM/yyyy')}</td>
+              <td>{formatter.format(operation.amount)}</td>
+              <td>{operation.document}</td>
+              <td>{operation.card}</td>
+              <td>{operation.hour}</td>
+              <td>{operation.storeOwner}</td>
+              <td>{operation.storeName}</td>
+            </tr>)
+          })
+        }
+        
       </table>
     </div>
   )
